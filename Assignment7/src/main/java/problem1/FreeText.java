7fb0bc19a5b5556ab731aa2abcb7780c3294fd06
Arjun-Patel <patel.arjun1@northeastern.com>
2022-03-27 17:32:48 -0700

    Updating classes for Arjun

--- Assignment7/src/main/java/problem1/FreeText.java
+++ Assignment7/src/main/java/problem1/FreeText.java
@@ -1,30 +1,103 @@
 package problem1;
 
+import java.util.Objects;
+
+/**
+ * FreeText: This validator will be used for free text fields such as messages or
+ * comments. Client code will provide the number of lines in the text field and the number
+ * of characters allowed per line. To be valid, input must be no longer than the number of
+ * lines multiplied by the number of characters allowed per line.
+ */
 public class FreeText implements Validator<String> {
-  public String input;
   public Integer numberOfLines;
   public Integer numberOfCharactersPerLine;
 
-  // constructor
-  // getters and setters
-  // equals and hashcode
-  // string
-
-  public boolean freeTextValid(String input){
-    int length = input.length();
-    int maximumLength = this.numberOfCharactersPerLine*this.numberOfCharactersPerLine;
-
-    if(length <=maximumLength){
-      return true;
-    }
-    else{
-      return false;
-    }
-
+  /**
+   * @param numberOfLines Number of lines in text box
+   * @param numberOfCharactersPerLine number of characters per line
+   */
+  public FreeText(Integer numberOfLines, Integer numberOfCharactersPerLine) {
+    this.numberOfLines = numberOfLines;
+    this.numberOfCharactersPerLine = numberOfCharactersPerLine;
   }
 
+  /**
+   * @return Integer: number of lines within object
+   */
+  public Integer getNumberOfLines() {
+    return this.numberOfLines;
+  }
+
+  /**
+   * @param numberOfLines Integer: number of lines within object
+   * set the number of lines within object
+   */
+  public void setNumberOfLines(Integer numberOfLines) {
+    this.numberOfLines = numberOfLines;
+  }
+
+  /**
+   * @return Integer: number of characters per line within object
+   */
+  public Integer getNumberOfCharactersPerLine() {
+    return this.numberOfCharactersPerLine;
+  }
+
+
+  /**
+   * @param numberOfCharactersPerLine Integer: Number of characters per line
+   * set the number of characters per line within object
+   */
+  public void setNumberOfCharactersPerLine(Integer numberOfCharactersPerLine) {
+    this.numberOfCharactersPerLine = numberOfCharactersPerLine;
+  }
+
+  /**
+   * @param o Object
+   * @return Boolean: States whether if 2 objects are the same
+   */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    FreeText freeText = (FreeText) o;
+    return Objects.equals(numberOfLines, freeText.numberOfLines)
+        && Objects.equals(numberOfCharactersPerLine, freeText.numberOfCharactersPerLine);
+  }
+
+  /**
+   * @return int: Hash code
+   */
+  @Override
+  public int hashCode() {
+    return Objects.hash(numberOfLines, numberOfCharactersPerLine);
+  }
+
+  /**
+   * @return String: String value of number of lines and number of characters per line
+   */
+  @Override
+  public String toString() {
+    return "FreeText{" +
+        "numberOfLines=" + numberOfLines +
+        ", numberOfCharactersPerLine=" + numberOfCharactersPerLine +
+        '}';
+  }
+
+  /**
+   * @param input - General user input to be checked
+   * @return A boolean to confirm whether the input is valid for
+   * FreeText
+   */
   @Override
   public boolean isValid(String input) {
-    return freeTextValid(input);
+    int length = input.length();
+    int maximumLength = this.numberOfLines*this.numberOfCharactersPerLine;
+
+    return length <=maximumLength;
   }
 }
--- Assignment7/src/main/java/problem1/Password.java
+++ Assignment7/src/main/java/problem1/Password.java
@@ -1,74 +1,191 @@
 package problem1;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
+import java.util.Objects;
+
+/**
+ * Password - A password is a String has the following properties:
+ * ○ Minimum and maximum acceptable length (inclusive).
+ * ○ The minimum number of lowercase letters that the password must contain
+ * (default = 0, which means that lowercase letters are not required).
+ * ○ The minimum number of uppercase letters that the password must contain
+ * (default = 0).
+ * ○ The minimum number of digits that the password must contain (default = 0).
+ * CS 5004, Spring 2022
+ * To be valid, a password must meet the length requirements and contain at least the
+ * minimum number of each character type. Additionally, a valid password cannot contain a
+ * space (“ “). To keep things simple, all other characters are allowed.
+ */
 public class Password implements Validator<String> {
   private Integer minimumLength;
   private Integer maximumLength;
-  private Integer lowerCase;
-  private Integer upperCase;
-  private Integer digits;
 
-  // constructor
-  // getters and setters
-  // equals and hashcode
-  // string
+  private static final int MIN_LOWER_CASE = 0;
+  private static final int MIN_UPPER_CASE = 0;
+  private static final int MIN_DIGITS =0;
 
 
+  /**
+   * @param minimumLength Minimum length of password
+   * @param maximumLength Maximum length of password
+   */
+  public Password(Integer minimumLength, Integer maximumLength) {
+    this.minimumLength = minimumLength;
+    this.maximumLength = maximumLength;
+  }
+
+  /**
+   * @return Integer: Minimum length within object
+   */
+  public Integer getMinimumLength() {
+    return this.minimumLength;
+  }
+
+  /**
+   * @param minimumLength Integer: Minimum length within object
+   * Set minimum length within object
+   */
+  public void setMinimumLength(Integer minimumLength) {
+    this.minimumLength = minimumLength;
+  }
+
+  /**
+   * @return Maximum length within object
+   */
+  public Integer getMaximumLength() {
+    return this.maximumLength;
+  }
+
+  /**
+   * @param maximumLength
+   */
+  public void setMaximumLength(Integer maximumLength) {
+    this.maximumLength = maximumLength;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Password password = (Password) o;
+    return Objects.equals(minimumLength, password.minimumLength)
+        && Objects.equals(maximumLength, password.maximumLength);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(minimumLength, maximumLength);
+  }
+
+  @Override
+  public String toString() {
+    return "Password{" +
+        "minimumLength=" + minimumLength +
+        ", maximumLength=" + maximumLength +
+        '}';
+  }
+
+  /**
+   * @param input A string for password
+   * @return A boolean to state whether the password has a valid length
+   * This is a helper method to isValid()
+   */
   public boolean isAcceptableLength(String input){
     Integer length = input.length();
     return (length>= minimumLength && length<= maximumLength);
 
   }
 
+  /**
+   * @param input A string for password
+   * @return A boolean to state whether the password has valid
+   * number of minimum lower cases
+   * This is a helper method to isValid()
+   */
   public boolean hasMinimumLowerCases(String input){
     Integer count = 0;
-    List<Character> list = new ArrayList<Character>(Integer.parseInt(input));
-    for(Character s : list) {
+    char [] list = new char[input.length()];
+
+    for(int i =0; i<input.length(); i++){
+      list[i] = input.charAt(i);
+    }
+
+    for(char s : list) {
       if (Character.isLowerCase(s)) {
         count++;
       }
     }
-    return count>=lowerCase;
+    return count>=MIN_LOWER_CASE;
   }
 
+  /**
+   * @param input A string for password
+   * @return A boolean to state whether the password has valid
+   * number of minimum upper cases
+   * This is a helper method to isValid()
+   */
   public boolean hasMinimumUpperCases(String input){
     Integer count = 0;
-    List<Character> list = new ArrayList<Character>(Integer.parseInt(input));
-    for(Character s : list) {
+    char [] list = new char[input.length()];
+
+    for(int i =0; i<input.length(); i++){
+      list[i] = input.charAt(i);
+    }
+
+    for(char s : list) {
       if (Character.isUpperCase(s)) {
         count++;
       }
     }
-    return (count>=upperCase);
+    return count>=MIN_UPPER_CASE;
   }
 
+  /**
+   * @param input A string for password
+   * @return A boolean to state whether the password has valid
+   * number of minimum digits
+   * This is a helper method to isValid()
+   */
   public boolean hasMinimumDigits(String input) {
     Integer count = 0;
-    List<Character> list = new ArrayList<Character>(Integer.parseInt(input));
-    for(Character s : list) {
+    char [] list = new char[input.length()];
+
+    for(int i =0; i<input.length(); i++){
+      list[i] = input.charAt(i);
+    }
+
+    for(char s : list) {
       if (Character.isDigit(s)) {
         count++;
       }
     }
-    return count>=digits;
+    return count>=MIN_DIGITS;
   }
 
-
-  public boolean hasNoSpace(String input){
-    Integer count = 0;
-    List<String> list = new ArrayList<String>(Collections.singleton(input));
-    for(String s : list) {
-      if (s == " ") {
+  /**
+   * @param input A string for password
+   * @return A boolean to state whether the password has
+   * no space
+   * This is a helper method to isValid()
+   */
+  public boolean hasNoSpace(String input) {
+    for (int i = 0; i < input.length(); i++) {
+      if (String.valueOf(input.charAt(i)).equals(" ") ) {
         return false;
       }
-    }
+      }
     return true;
-  }
+    }
 
-
+  /**
+   * @param input - General user input to be checked
+   * @return A boolean to confirm whether the input is valid for
+   * Password
+   */
   @Override
   public boolean isValid(String input) {
     return isAcceptableLength(input) &&
