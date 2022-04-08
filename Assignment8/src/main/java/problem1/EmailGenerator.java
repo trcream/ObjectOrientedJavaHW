8fd4b626ece13bae4054a47aaf53ad0fc39bb48c
Tcreamer <trentoncreamer@yahoo.com>
2022-04-08 05:36:36 -0700

    Progress push before pulling Arjuns code

--- Assignment8/src/main/java/problem1/EmailGenerator.java
+++ Assignment8/src/main/java/problem1/EmailGenerator.java
@@ -1,11 +1,14 @@
 package problem1;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class EmailGenerator extends FileGenerator {
+
 
   /**
    * @param csv       CSV file to derive information from to fill letter
@@ -21,38 +24,66 @@ public class EmailGenerator extends FileGenerator {
     ArrayList<String> parsedArray = new ArrayList<>();
     System.out.println("Printing from Email Generator");
     parsedArray = super.parseCsv(fileLocation);
+    ArrayList<String> parsedValues = new ArrayList<>();
     for(int i = 0; i< parsedArray.size(); i++){
       String[] list = parsedArray.get(i).split("\",\"");
-      System.out.println("First name: " + list[0].replaceAll("\"", ""));
-      System.out.println("Last name: " + list[1].replaceAll("\"", ""));
-      System.out.println("Email: " + list[10].replaceAll("\"", ""));
+      //System.out.println("First name: " + list[0].replaceAll("\"", ""));
+      //System.out.println("Last name: " + list[1].replaceAll("\"", ""));
+      //System.out.println("Email: " + list[10].replaceAll("\"", ""));
+      parsedValues.add(list[0].replaceAll("\"", ""));
+      parsedValues.add(list[1].replaceAll("\"", ""));
+      parsedValues.add(list[10].replaceAll("\"", ""));
+      //helper method
+      writeFileContent("C:\\\\Users\\\\trent\\\\Desktop\\\\Masters Programs\\\\Northeastern\\\\CS5004\\\\GroupProject\\\\Team_repo_Repo6_Arjun_Matthew_Trenton\\\\Assignment8\\\\src\\\\main\\\\java\\\\problem1\\\\email-template.txt\"",
+          list[0].replaceAll("\"", ""),
+          list[1].replaceAll("\"", ""),
+          list[10].replaceAll("\"", ""));
     }
-    return parsedArray;
-
+    return parsedValues;
   }
 
-  public void writeFileContent(String fileLocation){
+
+  public void writeFileContent(String fileLocation, String firstName, String lastName, String email ){
+    fileLocation = "C:\\Users\\trent\\Desktop\\Masters Programs\\Northeastern\\CS5004\\GroupProject\\Team_repo_Repo6_Arjun_Matthew_Trenton\\Assignment8\\src\\main\\java\\problem1\\email-template.txt";
+
     try {
       BufferedReader reader = new BufferedReader(new FileReader(fileLocation));
+      BufferedWriter writer = new BufferedWriter(new FileWriter("testing.txt"));
+
       String line;
 
       while((line = reader.readLine()) != null){
-       String[] tempArr = line.split(" ");
-        for(int i =0; i< tempArr.length; i++){
-          System.out.println(tempArr[i].replaceAll("\\[\\[email", "").replaceAll("]]", "trentoncreamer@yahoo.com"));
-          //System.out.println(tempArr[i].replaceAll("\\\\[|\\\\]", "trentcreamer@yahoo.com"));
+        //System.out.println(line.replaceAll("\\[\\[email", "").replaceAll("]]", "trentoncreamer@yahoo.com"));
+        line = line.replaceAll("\\[\\[emai", "").replaceAll("l]]", firstName);
+        //System.out.println(line.replaceAll("\\[\\[first_name", "").replaceAll("]]", "trenton"));
+        line = line.replaceAll("\\[\\[fi", "").replaceAll("rst_name]]", lastName);
+        //System.out.println(line.replaceAll("\\[\\[last_name", "").replaceAll("]]", "Creamer"));
+        line = line.replaceAll("\\[\\[l", "").replaceAll("ast_name]]", email);
 
-//          System.out.println(tempArr[i].replaceAll("first_name", "Trenton"));
-//          System.out.println(tempArr[i].replaceAll("last_name", "Creamer"));
-        }
+        writer.write(line);
+        writer.newLine();
 
       }
       reader.close();
+      writer.close();
     } catch (IOException e) {
       e.printStackTrace();
     }
 
   }
+
+  public void writeEmail(String location, String line){
+    location = "testing.txt";
+    try{
+      BufferedWriter writer = new BufferedWriter(new FileWriter(location));
+      writer.write(line);
+      writer.close();
+    }catch(IOException e){
+      e.printStackTrace();
+
+    }
+  }
+
 
 //  @Override
 //  createFileName(){
