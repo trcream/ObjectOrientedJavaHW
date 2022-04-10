ac551bc0c5e709eab98591de74e7b1f6d81ccdbd
Tcreamer <trentoncreamer@yahoo.com>
2022-04-10 13:22:20 -0700

    Added a file generator interfece

--- Assignment8/src/main/java/problem1/FileGenerator.java
+++ Assignment8/src/main/java/problem1/FileGenerator.java
@@ -5,10 +5,13 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public abstract class FileGenerator {
+/**
+ * File generator is an abstract class that is used to generate files
+ */
+public abstract class FileGenerator implements FileGeneratorI{
 
   /**
-   *
+   * Abstract constructor for the file generator class
    * @param csv CSV file to derive information from to fill letter
    * @param template Template file to use to generate letter
    * @param outputDirPath Output directory to save newly generated letter to.
@@ -18,15 +21,25 @@ public abstract class FileGenerator {
 
   }
 
-  public void createFileName(){
+  /**
+   * Method to create File Names
+   */
+
+  public String createFileName(){
     // return "firstname_lastname.txt
+    return "";
   }
 
-  public ArrayList<String> parseCsv(String fileLocation){
+  /**
+   * Method to parse the line values from a given csv file
+   * @param csv - file location of the csv to be parsed
+   * @return - Array list of parsed csv values
+   */
+  public ArrayList<String> parseCsv(String csv){
     // parse CSV file to get information, need to match data to column name
     ArrayList<String> parsedArray = new ArrayList<>();
     try{
-      BufferedReader reader = new BufferedReader(new FileReader(fileLocation));
+      BufferedReader reader = new BufferedReader(new FileReader(csv));
       String line;
       while ((line = reader.readLine()) != null) {
         parsedArray.add(line);
@@ -38,6 +51,21 @@ public abstract class FileGenerator {
     }
     return parsedArray;
   }
+
+  /**
+   * Method to set the index values for the requested header fields
+   * @param csv - CSV to set the header values to
+   */
+  public void setHeaderValues(String csv) {
+
+  }
+
+  /**
+   * Method to generate emails based upon file input and desired output location
+   * @param csv - csv file to parse user data from
+   * @param template - template to insert user data into
+   * @param outputDirPath - output path to store generated files
+   */
 
   public void generateFileContent(String csv, String template, String outputDirPath) {
   }
