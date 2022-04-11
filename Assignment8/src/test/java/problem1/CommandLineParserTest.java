b0dcdc306b17abc7c6f9302dbf539adce52e31cc
matthfox <88064362+matthfox@users.noreply.github.com>
2022-04-10 21:47:46 -0700

    Adding test files

---
+++ Assignment8/src/test/java/problem1/CommandLineParserTest.java
@@ -0,0 +1,17 @@
+package problem1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CommandLineParserTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void checkRequiredOptions() {
+  }
+}
