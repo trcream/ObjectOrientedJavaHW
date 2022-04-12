f3fb43c222e28e29de22367b54e4aea20d05155e
Arjun-Patel <patel.arjun1@northeastern.com>
2022-04-11 22:55:17 -0700

    Updating files Arjun

--- Assignment8/src/test/java/problem1/LetterGeneratorTest.java
+++ Assignment8/src/test/java/problem1/LetterGeneratorTest.java
@@ -6,6 +6,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.junit.jupiter.api.BeforeEach;
@@ -22,14 +24,44 @@ class LetterGeneratorTest {
   @BeforeEach
   void setUp() {
     // Type in your file locations here
-    String CSV = "C:\\Users\\Arjun Patel\\IdeaProjects\\Team_repo_Repo6_Arjun_Matthew_Trenton\\Assignment8\\src\\main\\java\\problem1\\insurance-company-members.csv";
-    String template = "C:\\Users\\Arjun Patel\\IdeaProjects\\Team_repo_Repo6_Arjun_Matthew_Trenton\\Assignment8\\src\\main\\java\\problem1\\letter-template.txt";
-    String output = "C:\\Users\\Arjun Patel\\IdeaProjects\\Team_repo_Repo6_Arjun_Matthew_Trenton\\Assignment8\\src\\main\\java\\file";
+    String CSV = "C:\\Users\\Arjun Patel\\IdeaProjects\\Team_repo_Repo6_Arjun_Matthew_Trenton\\Assignment8\\src\\main\\java\\inputFiles\\insurance-company-members.csv";
+    String template = "C:\\Users\\Arjun Patel\\IdeaProjects\\Team_repo_Repo6_Arjun_Matthew_Trenton\\Assignment8\\src\\main\\java\\inputFiles\\letter-template.txt";
+    String output = "C:\\Users\\Arjun Patel\\IdeaProjects\\Team_repo_Repo6_Arjun_Matthew_Trenton\\Assignment8\\src\\main\\java\\outputFiles";
     testLetterGenerator = new LetterGenerator(CSV,template,output);
-//    testFileGenerator = new FileGenerator(CSV,template,output);
-//    testFileGenerator.parseCsv(CSV);
-//    testFileGenerator2 = new FileGenerator(CSV,template,output);
+
   }
+
+  @Test
+  void getCsv() {
+    assertEquals("C:\\Users\\Arjun Patel\\IdeaProjects\\Team_repo_Repo6_Arjun_Matthew_Trenton\\Assignment8\\src\\main\\java\\inputFiles\\insurance-company-members.csv",testLetterGenerator.getCsv());
+
+  }
+
+  @Test
+  void getTemplate() {
+    assertEquals("C:\\Users\\Arjun Patel\\IdeaProjects\\Team_repo_Repo6_Arjun_Matthew_Trenton\\Assignment8\\src\\main\\java\\inputFiles\\letter-template.txt",testLetterGenerator.getTemplate());
+
+  }
+
+  @Test
+  void getOutputDirPath() {
+    assertEquals("C:\\Users\\Arjun Patel\\IdeaProjects\\Team_repo_Repo6_Arjun_Matthew_Trenton\\Assignment8\\src\\main\\java\\outputFiles",testLetterGenerator.getOutputDirPath());
+
+  }
+
+  @Test
+  void getInformationFromCsv() {
+    testLetterGenerator.parseCsv(CSV);
+    testLetterGenerator.getInformationFromCsv();
+
+  }
+
+  @Test
+  void getColumnIndices() {
+    testLetterGenerator.getColumnIndices();
+
+  }
+
 
   @Test
   void createFileName() {
@@ -42,36 +74,29 @@ class LetterGeneratorTest {
   @Test
   void generateDefaultFileName() {
     testLetterGenerator.parseCsv(CSV);
-    assertEquals("",testFileGenerator.generateDefaultFileName(0));
+    //assertEquals("",testFileGenerator.generateDefaultFileName(1));
 
   }
 
-//  @Test
-//  void createColumnIndices() {
-      // This cannot return void
-      // This should return an integer
-//    assertEquals(0,testFileGenerator.createColumnIndices());
-//
-//  }
+  @Test
+  void createColumnIndices() {
+    testLetterGenerator.parseCsv(CSV);
+    testLetterGenerator.createColumnIndices();
+
+  }
 
   @Test
   void parseCsv() {
-    // This cannot be void. This needs to return an Array List
-//    testLetterGenerator.parseCsv(CSV);
-//
-//    //assertEquals("",testFileGenerator2);
-//
-//    assertEquals(0,testLetterGenerator.writeFileContent(1,"zztest"));
-//
 
   }
 
-//  @Test
-//  void writeFileContent() {
-//    testFileGenerator.writeFileContent(0,CSV);
-//    assertEquals("",testFileGenerator);
-//
-//  }
+  @Test
+  void writeFileContent() {
+    testLetterGenerator.parseCsv(CSV);
+
+  }
+
+
 
   @Test
   void generate() throws IOException {
@@ -81,28 +106,39 @@ class LetterGeneratorTest {
     // Test to see if length of CSV matches expected
     assertEquals(501, testLetterGenerator.informationFromCsv.size());
 
-    //Test to see if files are generated
-    assertEquals(1, testLetterGenerator.writeFileContent(1, "zztest"));
-    assertEquals(1, testLetterGenerator.writeFileContent(500, "zztest2"));
+    // Generate Files
+    testLetterGenerator.writeFileContent(1, "zztest");
+    testLetterGenerator.writeFileContent(500, "zztest2");
+
 
     // Test to see if files are formatted correctly
-    // This one should be generated as zztest2
-    BufferedReader reader = new BufferedReader(new FileReader(
-        "C:\\Users\\Arjun Patel\\IdeaProjects\\Team_repo_Repo6_Arjun_Matthew_Trenton\\Assignment8\\src\\main\\java\\file\\zztest2"));
+    // This one should be generated as zztest2 and compare it to expected
+    BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Arjun Patel\\IdeaProjects\\Team_repo_Repo6_Arjun_Matthew_Trenton\\Assignment8\\src\\main\\java\\outputFiles\\zztest2"));
 
     // This one you need to get the last file called Chauncey_Motley.txt
     BufferedReader reader2 = new BufferedReader(new FileReader(
-        "C:\\Users\\Arjun Patel\\IdeaProjects\\Team_repo_Repo6_Arjun_Matthew_Trenton\\Assignment8\\src\\main\\java\\file\\Chauncey_Motley.txt"));
+        "C:\\Users\\Arjun Patel\\IdeaProjects\\Team_repo_Repo6_Arjun_Matthew_Trenton\\Assignment8\\src\\main\\java\\outputFiles\\Chauncey_Motley_letter.txt"));
     String line;
     String line2;
 
     while (((line = reader.readLine()) != null) && ((line2 = reader2.readLine()) != null)) {
       assertEquals(line2, line);
     }
+
+
+
+
   }
 
   @Test
   void testCreateFileName() {
-    assertEquals("file_0.txt",testFileGenerator.createFileName(0));
+//    testLetterGenerator.parseCsv(CSV);
+//    testFileGenerator.createFileName(1);
   }
+
+
+
+
+
+
 }
