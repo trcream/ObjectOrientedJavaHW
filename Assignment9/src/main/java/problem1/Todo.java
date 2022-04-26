7f49c8b8adb84ebce0a0c73d8895d11daeaacc96
Tcreamer <trentoncreamer@yahoo.com>
2022-04-25 19:17:03 -0700

    Added functionality code and refactored code for errors

--- Assignment9/src/main/java/problem1/Todo.java
+++ Assignment9/src/main/java/problem1/Todo.java
@@ -1,65 +1,69 @@
 package problem1;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 public class Todo {
-  private Integer id;
-  private String text;
-  private Boolean completed;
-  private LocalDateTime due;
-  private Integer priority;
-  private String category;
 
-  public Todo(Integer id, String text, LocalDateTime due, Integer priority, String category) {
+  public Integer id;
+  public String text;
+  public Boolean completed;
+  public LocalDate dueDate;
+  public Integer priority;
+  public String category;
+
+  public Todo(Integer id, String text, Boolean completed, LocalDate dueDate,
+      Integer priority, String category) {
     this.id = id;
     this.text = text;
-    this.due = due;
-    this.priority = priority;
-    this.category = category;
-    this.completed = Boolean.FALSE;
-  }
-
-  public Todo(Integer id, String text, LocalDateTime due, Integer priority, String category, Boolean completed) {
-    this.id = id;
-    this.text = text;
-    this.due = due;
-    this.priority = priority;
-    this.category = category;
     this.completed = completed;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.category = category;
   }
 
-  public Todo(Integer id, String text) {
+  public void setId(Integer id) {
     this.id = id;
+  }
+
+  public void setText(String text) {
     this.text = text;
-    // Change "3" to this.DEFAULT_PRIORITY
-    this.priority = 3;
-  }
-
-  public Integer getId() {
-    return this.id;
-  }
-
-  public String getText() {
-    return this.text;
-  }
-
-  public LocalDateTime getDue() {
-    return this.due;
-  }
-
-  public Integer getPriority() {
-    return this.priority;
-  }
-
-  public String getCategory() {
-    return this.category;
-  }
-
-  public Boolean getCompleted() {
-    return this.completed;
   }
 
   public void setCompleted(Boolean completed) {
     this.completed = completed;
   }
+
+  public void setDueDate(LocalDate dueDate) {
+    this.dueDate = dueDate;
+  }
+
+  public void setPriority(Integer priority) {
+    this.priority = priority;
+  }
+
+  public void setCategory(String category) {
+    this.category = category;
+  }
 }
+
+
+
+//  public Todo(Integer id, String text, LocalDateTime due, Integer priority, String category) {
+//    this.id = id;
+//    this.text = text;
+//    this.due = due;
+//    this.priority = priority;
+//    this.category = category;
+//    this.completed = Boolean.FALSE;
+//  }
+//
+//  public Todo(Integer id, String text, LocalDateTime due, Integer priority, String category, Boolean completed) {
+//    this.id = id;
+//    this.text = text;
+//    this.due = due;
+//    this.priority = priority;
+//    this.category = category;
+//    this.completed = completed;
+//  }
+
+
