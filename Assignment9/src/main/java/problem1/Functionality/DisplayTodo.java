1c36655d87d4baa8c486baa0f0fbb1323f539cd9
Arjun-Patel <patel.arjun1@northeastern.com>
2022-04-26 17:46:09 -0700

    pushing up files

--- Assignment9/src/main/java/problem1/Functionality/DisplayTodo.java
+++ Assignment9/src/main/java/problem1/Functionality/DisplayTodo.java
@@ -1,16 +1,51 @@
 package problem1.Functionality;
 
 import java.util.*;
+import problem1.CSVParser;
 import problem1.Todo;
 
 public class DisplayTodo implements Functionality<ArrayList<String>> {
-  public HashMap<String, Todo> todos;
 
-  public DisplayTodo(HashMap<String, Todo> todos) {
-    this.todos = todos;
+
+
+  public void displayToDo() {
+    Integer counter = 0;
+    while (true) {
+      if (CSVParser.getToDoList().get(counter) != null) {
+        System.out.println(CSVParser.getToDoList().get(counter));
+      }
+    }
   }
 
-  public void call(ArrayList<String> displayOptions) {
-    System.out.println("Display Todo list");
+  public void showIncomplete() {
+   Integer counter = 0;
+   while (true) {
+    if (CSVParser.getToDoList().get(counter).completed == false) {
+      System.out.println(CSVParser.getToDoList().get(counter));
+    }
+   }
   }
+
+  public void sortByDate() {
+    Integer counter = 0;
+    while (true) {
+      if (CSVParser.getToDoList().get(counter).completed == false) {
+        System.out.println(CSVParser.getToDoList().get(counter));
+      }
+    }
+  }
+
+  public void sortByPriority() {
+    Integer counter = 0;
+    while (true) {
+      if (CSVParser.getToDoList().get(counter).completed == false) {
+        System.out.println(CSVParser.getToDoList().get(counter));
+      }
+    }
+  }
+
+
+
+
+
 }
