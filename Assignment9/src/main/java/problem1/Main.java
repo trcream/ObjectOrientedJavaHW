55574ac0ffab6ea95d73c2e739412530df9cbd6c
matthfox <88064362+matthfox@users.noreply.github.com>
2022-04-27 21:20:27 -0700

    Fixed main and add write csv functionality

--- Assignment9/src/main/java/problem1/Main.java
+++ Assignment9/src/main/java/problem1/Main.java
@@ -1,17 +1,12 @@
 package problem1;
 
-<<<<<<< HEAD
 import java.io.File;
-=======
-
->>>>>>> main
 public class Main {
   /**
    * Main method for running program and handling arguments from the user
    * @param args - arguments provided by the user
    */
   public static void main(String[] args) {
-<<<<<<< HEAD
     TodoApp app = new TodoApp();
 
     String filePath = new File("").getAbsolutePath();
@@ -46,17 +41,5 @@ public class Main {
 
     //  TODO: REMOVE TESTS ABOVE AND UNCOMMENT BELOW
     // app.run(args);
-=======
-//     TodoApp app = new TodoApp();
-//     String[] testArgs = {"--csv-file", "path/to/file","--add-todo", "--todo-text", "Test todo", "--todo-text", "Test todo 2", "--priority", "3"};
-//     app.run(testArgs);
-
-    CSVParser testCSVParser = new CSVParser("C:\\Users\\trent\\Desktop\\Masters Programs\\Northeastern\\CS5004\\GroupProject\\Team_repo_Repo6_Arjun_Matthew_Trenton\\Assignment9\\src\\main\\java\\problem1\\todos.csv", "");
-    testCSVParser.parseCsv("C:\\Users\\trent\\Desktop\\Masters Programs\\Northeastern\\CS5004\\GroupProject\\Team_repo_Repo6_Arjun_Matthew_Trenton\\Assignment9\\src\\main\\java\\problem1\\todos.csv");
-    //System.out.println("Printing from main " + testCSVParser.toDoList.get(5).text);
-    DisplayTodo testDisplay = new DisplayTodo();
-    testDisplay.displayToDo();
-
->>>>>>> main
   }
 }
