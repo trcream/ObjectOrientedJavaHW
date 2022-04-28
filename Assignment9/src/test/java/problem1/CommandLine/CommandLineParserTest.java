38df1d5c43bd46a6a510865586ccd8a7158b1438
Tcreamer <trentoncreamer@yahoo.com>
2022-04-28 16:41:32 -0700

    Added command line parser test

--- Assignment9/src/test/java/problem1/CommandLine/CommandLineParserTest.java
+++ Assignment9/src/test/java/problem1/CommandLine/CommandLineParserTest.java
@@ -2,6 +2,40 @@ package problem1.CommandLine;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class CommandLineParserTest {
+  private CommandLineParser testCommandLineParser;
+
+  @BeforeEach
+  void setUp() {
+    ArrayList<ParserArgument> arguments = new ArrayList();
+    String argumentPrefix = "--";
+
+    testCommandLineParser = new CommandLineParser(arguments , argumentPrefix);
+  }
+
+  @Test
+  void getArguments() {
+    assertEquals(0, testCommandLineParser.getArguments().size());
+  }
+
+  @Test
+  void getArgumentPrefix() {
+    assertEquals("--", testCommandLineParser.getArgumentPrefix());
+  }
+
+  @Test
+  void getProcessedArgs() {
+    assertEquals(0, testCommandLineParser.getProcessedArgs().size());
+  }
+
+  @Test
+  void getArgumentsMap() {
+    assertEquals(0, testCommandLineParser.getArgumentsMap().size());
+
+  }
 
 }
