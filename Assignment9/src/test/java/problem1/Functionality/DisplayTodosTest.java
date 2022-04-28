7504a6ce6216f58eb4c54cd1f750702fc1b359b9
Tcreamer <trentoncreamer@yahoo.com>
2022-04-28 13:55:24 -0700

    Adding the CompleteTodoTEst and DisplayTodos test

--- Assignment9/src/test/java/problem1/Functionality/DisplayTodosTest.java
+++ Assignment9/src/test/java/problem1/Functionality/DisplayTodosTest.java
@@ -1,11 +1,55 @@
 package problem1.Functionality;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+
+import java.io.File;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import problem1.CommandLine.CommandLineParser;
+import problem1.TodoApp;
 
 class DisplayTodosTest {
+
+  TodoApp app = new TodoApp();
+  String filePath = new File("").getAbsolutePath();
+  String testCsv = filePath + "/src/main/java/problem1/todos.csv";
+
+  String[] testArgs = {
+      "--csv-file", testCsv,
+      "--add-todo",
+      "--todo-text", "Test todo",
+      "--completed",
+      "--due", "3/21/2022",
+      "--priority", "2",
+      "--display",
+      "--show-incomplete",
+      "--show-category", "home",
+      "--sort-by-date",
+      "--complete-todo", "2",
+      "--complete-todo", "3"
+  };
+
+  String[] test2Args = {
+      "--csv-file", testCsv,
+      "--add-todo",
+      "--todo-text", "Test todo",
+      "--completed",
+      "--due", "3/21/2022",
+      "--priority", "2",
+      "--display",
+      "--sort-by-priority"
+  };
+
+  String[] test3Args = {
+      "--csv-file", testCsv,
+      "--add-todo",
+      "--todo-text", "Test todo",
+      "--completed",
+      "--due", "3/21/2022",
+      "--priority", "2",
+      "--display",
+      "--sort-by-date"
+  };
 
   @BeforeEach
   void setUp() {
@@ -13,5 +57,8 @@ class DisplayTodosTest {
 
   @Test
   void run() {
+    app.run(testArgs);
+    app.run(test2Args);
+    app.run(test3Args);
   }
 }
